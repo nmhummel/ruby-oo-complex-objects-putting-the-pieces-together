9fd48fd14c20636eaaf3a56a77eb1148c44087db
nmhummel <natalie.hummel@gmail.com>
2020-10-29 18:23:34 -0500

    commit tree 6ad9dc47dfb53f2db3ff421bb56bc0ea8d7e555e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
 
+class Book
 
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+
+    def initialize(title)
+        @title = title 
+        #instance variable - any method within an instance has access to any instance variable
+        #so, within our instances, we don't need to worry about passing variables around
+        #think of instance variables as properties of any particular instance of a class
+    end
+
+    def title #getter/reader
+        @title 
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
